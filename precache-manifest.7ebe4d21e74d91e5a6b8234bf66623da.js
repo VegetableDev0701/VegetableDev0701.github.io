165e822cb7fc77c1e9de7b9c52d0ad2ed2eaa07d
VegetableDev0701 <dreamsky0701@hotmail.com>
2024-01-19 02:36:11 -0800

    feat: init project

---
+++ precache-manifest.7ebe4d21e74d91e5a6b8234bf66623da.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c0fef5d71a74fc3c427",
+    "url": "/static/css/main.bbaab850.chunk.css"
+  },
+  {
+    "revision": "5c0fef5d71a74fc3c427",
+    "url": "/static/js/main.5c0fef5d.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5ffaeb0a4e5e4e784d50",
+    "url": "/static/css/2.be900d9f.chunk.css"
+  },
+  {
+    "revision": "5ffaeb0a4e5e4e784d50",
+    "url": "/static/js/2.5ffaeb0a.chunk.js"
+  },
+  {
+    "revision": "5e22511b71a7aab026fe372382f9ac20",
+    "url": "/static/media/me.5e22511b.jpg"
+  },
+  {
+    "revision": "1ad917eea8b4c921992d925cac64490f",
+    "url": "/static/media/raleway-bold-webfont.1ad917ee.woff2"
+  },
+  {
+    "revision": "474c1ebf2a0a463d9a03a29a065ae8f2",
+    "url": "/static/media/raleway-bold-webfont.474c1ebf.woff"
+  },
+  {
+    "revision": "dda65b551e6241174edd384d2a44085b",
+    "url": "/static/media/raleway-regular-webfont.dda65b55.woff"
+  },
+  {
+    "revision": "50a248dc33ced2bbd235a7a9a473825b",
+    "url": "/static/media/raleway-regular-webfont.50a248dc.woff2"
+  },
+  {
+    "revision": "1d574968db21bd1eb692175bbe77dd32",
+    "url": "/static/media/raleway-semibold-webfont.1d574968.woff2"
+  },
+  {
+    "revision": "34173a45af2c9b30a252fbe77c5364c8",
+    "url": "/static/media/raleway-semibold-webfont.34173a45.woff"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a04e2e18b99b2cd991133060170288dc",
+    "url": "/index.html"
+  }
+];
